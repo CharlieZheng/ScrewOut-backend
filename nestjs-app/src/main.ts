72856e541ffb3f7903e60125b6ff0f04fa589be2
hanrong.zheng <transsionoraimohanrong@gmail.com>
2026-02-10 17:30:48 +0800

    https 证书

--- nestjs-app/src/main.ts
+++ nestjs-app/src/main.ts
@@ -1,9 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import {UserModule} from "./module/UserModule";
-
+import * as fs from 'fs';
+import * as path from 'path';
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+    // 1. 读取证书文件
+    // 注意：如果是 Docker 运行，这里的路径是容器内的路径
+    const httpsOptions = {
+        key: fs.readFileSync(path.join(__dirname, '../certs/screw-out.gg-bond.fun.key')),
+        cert: fs.readFileSync(path.join(__dirname, '../certs/screw-out.gg-bond.fun.pem')),
+    };
+    const app = await NestFactory.create(AppModule,{httpsOptions});
+
+    await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
