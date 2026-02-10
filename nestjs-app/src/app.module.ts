18deac6e36f65f906d5947ebe3e3ab4ed286bdaa
hanrong.zheng <transsionoraimohanrong@gmail.com>
2026-02-10 11:50:09 +0800

    把 UserModule import 进来

--- nestjs-app/src/app.module.ts
+++ nestjs-app/src/app.module.ts
@@ -8,7 +8,16 @@ import {TypeOrmModule} from "@nestjs/typeorm";
 import {UserModule} from "./module/UserModule";
 
 @Module({
-    imports: [  UserModule],
+    imports: [ TypeOrmModule.forRoot({
+        type: 'mysql',
+        host: 'db',          // docker 用 db，本地可能是 localhost
+        port: 3306,
+        username: 'root',
+        password: 'password123',
+        database: 'screw_out',
+        entities: [User],
+        synchronize: true,
+    }), UserModule],
     controllers: [AppController],
     providers: [AppService],
 })
