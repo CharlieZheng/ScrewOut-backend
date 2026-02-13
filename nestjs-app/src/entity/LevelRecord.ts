ef4cb189186530337d8fd1a463df8c5bf3b83569
hanrong.zheng <transsionoraimohanrong@gmail.com>
2026-02-13 16:20:08 +0800

    LevelRecord、UserGameProgress 依赖 User

--- nestjs-app/src/entity/LevelRecord.ts
+++ nestjs-app/src/entity/LevelRecord.ts
@@ -3,22 +3,39 @@
  * 1. 数据库实体定义 (建议存放于 user.entity.ts)
  * ==========================================
  */
-import {Entity, Column, PrimaryColumn, CreateDateColumn, UpdateDateColumn} from 'typeorm';
+import {
+    Entity,
+    Column,
+    PrimaryColumn,
+    CreateDateColumn,
+    UpdateDateColumn,
+    ManyToOne,
+    JoinColumn,
+    OneToOne
+} from 'typeorm';
 import * as trace_events from "node:trace_events";
+import {User} from "./User";
 
 @Entity('level_record')
 export class LevelRecord {
     @PrimaryColumn()
     id:number  ;
-    @Column()
-    user_id:number  ;
+    @OneToOne(() => User, {
+        onDelete: 'CASCADE',
+    })
+    @JoinColumn({ name: 'user_id' })
+    user: User;
     @Column()
     level_index: number ;
     @Column()
     game_start_time: number ;
     @Column()
     game_end_time: number ;
-    @Column()
+    @Column({
+        type: 'int',
+        asExpression: 'TIMESTAMPDIFF(SECOND, game_start_time, game_end_time)',
+        generatedType: 'STORED',  // 注意 STORED
+    })
     time_cost_in_seconds: number ;
 
 
--- nestjs-app/src/entity/UserGameProgress.ts
+++ nestjs-app/src/entity/UserGameProgress.ts
@@ -3,15 +3,20 @@
  * 1. 数据库实体定义 (建议存放于 user.entity.ts)
  * ==========================================
  */
-import {Entity, Column, PrimaryColumn, CreateDateColumn, UpdateDateColumn} from 'typeorm';
+import {Entity, Column, PrimaryColumn, CreateDateColumn, UpdateDateColumn, ManyToOne, JoinColumn} from 'typeorm';
 import * as trace_events from "node:trace_events";
+import {User} from "./User";
 
 @Entity('user_game_progress')
 export class UserGameProgress {
-    @Column()
-    user_id:number  ;
+
+
+    @ManyToOne(() => User, {
+        onDelete: 'CASCADE',
+    })
+    @JoinColumn({ name: 'user_id' })
+    user: User;
     @Column()
     current_level_index: number ;
-
 
 }
